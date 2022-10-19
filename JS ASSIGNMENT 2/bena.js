9a95e3726748f26c2a1c39a08379204225206dbe
musiime0 <mbenah25@gmail.com>
2022-10-19 18:07:46 +0300

    JS assignment 2

---
+++ JS ASSIGNMENT 2/bena.js
@@ -0,0 +1,24 @@
+//Array
+//For loop
+var names = ["Nagasha","Glenn","Pius","Benah"]
+
+for(i = 0; i< names.length; i++) {
+    if(names[i]="Benah") {
+        console.log("this is my name" + names[i])
+    }
+}
+
+//While loop
+var i = 1;
+while(i<=4){
+    console.log(i +"<br/>");
+    i++;
+}
+
+//For each loop
+let names =[Nagasha,Glenn,Pius,Benah];
+names.foreach((n,i,names) => {
+    onslotchange.log(n,i,names);
+});
+console.log(names);
+
